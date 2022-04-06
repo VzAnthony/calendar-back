9390c0e08477856e0fd4c8739042f45a41cea2e5
Anthony <anthony560@hotmail.com>
2022-04-06 19:06:47 +0200

    fixed in renew

--- controllers/auth.js
+++ controllers/auth.js
@@ -100,7 +100,9 @@ const tokenRefresh = async (req , res = response) => {
     res.json({
         ok: true,
         msg: 'renew',
-        token
+        token,
+        uid,
+        name
     })
 }
 
